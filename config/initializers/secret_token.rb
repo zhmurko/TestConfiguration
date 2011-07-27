126ed594ba887c9145ef2900d468dea62b83ab4f
Dmitry Zhmurko <iamfm@fedora.fm.local>
2011-07-27 21:41:21 +0300

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZhmurkoTest::Application.config.secret_token = '2e0262d07c310789ab3b7997ff6f5205a319647e8434ebdbb5be4121a2f270cbdeb85e93ad609440fd3096438f196677363d614b672145ee2c946d0e409814db'
