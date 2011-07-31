64008da1e76d33180a61ae6dab6676d88d954881
Dmitry Zhmurko <iamfm@fedora.fm.local>
2011-08-01 02:19:30 +0300

    move logic to settings from configuration controller

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ ZhmurkoTest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "configuration#index"
+  root :to => "settings#index"
 
   # See how all your routes lay out with "rake routes"
 
