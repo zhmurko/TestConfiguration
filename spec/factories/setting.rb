d691b4a55c1185749acb84cacad55e12b1e8c9b7
Dmitry Zhmurko <iamfm@fedora.fm.local>
2011-08-01 02:20:42 +0300

    fix sequence in Factory setting

--- spec/factories/setting.rb
+++ spec/factories/setting.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
   factory :setting do |s|
-    caption 
+    caption
     s.value    "value"
     s.web_type "input"
-    s.after_build { |s| s.save! }
   end
   sequence :caption do |n|
     "name#{n}"
