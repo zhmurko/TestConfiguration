8e42d795b55c29c55c68f66bd865140008bd646c
Dmitry Zhmurko <iamfm@fedora.fm.local>
2011-08-01 02:22:41 +0300

    adjust views of Settings

--- spec/views/configuration/index.html.haml_spec.rb
+++ spec/views/configuration/index.html.haml_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe "configuration/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do
+    @settings = assign(:setting, FactoryGirl.build_list(:setting, 20))
+  end
+  it "should render all settings" do
+    render
+    rendered.should =~ /name20/
+  end
 end
