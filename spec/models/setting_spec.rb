9a3b0bf6058b4cbd6b4f4db15fd126f4e889774b
Dmitry Zhmurko <iamfm@fedora.fm.local>
2011-07-31 18:30:00 +0300

    specs for Setting

--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -1,11 +1,38 @@
 require 'spec_helper'
 
 describe Setting do
-  before :each do
-    @setting = Setting.new
+  let(:setting) { Factory(:setting) }
+  subject { setting }
+
+  describe "has fields" do
+    it { should respond_to :caption }
+    it { should respond_to :value }
+    it { should respond_to :web_type }
+    it { should respond_to :converted_value }
   end
 
-  it "should have a value" do
+  context "returns" do
+
+    context "string" do
+      let(:setting) { Factory :setting, :value => "just a string"  }
+      its(:converted_value) { should be_kind_of(String) }
+    end
+
+    context "integer" do
+      let(:setting) { Factory :setting, :value => 1 }
+      its(:converted_value) { should be_kind_of(Integer) }
+    end
+
+    context "just a date" do
+      let(:setting) { Factory :setting, :value => "01/01/1982" }
+      its(:converted_value) { should be_kind_of(Date) }
+    end
+
+    context "date with time" do
+      let(:setting) { Factory :setting, :value => "01/01/1982 17:32" }
+      its(:converted_value) { should be_kind_of(DateTime) }
+    end
+
   end
-  
+
 end
